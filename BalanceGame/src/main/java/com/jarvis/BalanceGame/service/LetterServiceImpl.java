2df8c9b1a66c76b5848cda077b96b9d1dee19791
hyun9-9 <pwc1088@naver.com>
2024-03-14 15:10:25 +0900

    에러 부분수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/service/LetterServiceImpl.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/service/LetterServiceImpl.java
@@ -1,8 +1,42 @@
 package com.jarvis.BalanceGame.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.jarvis.BalanceGame.model.dto.LetterDTO;
+
 @Service
-public class LetterServiceImpl {
+public class LetterServiceImpl implements LetterService{
+
+	@Override
+	public boolean insert() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<LetterDTO> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LetterDTO selectOne() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
