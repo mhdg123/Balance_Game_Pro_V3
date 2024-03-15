da3fedc8bb3681bc288c09d93e0d096bc96752ff
chanwoo1997 <hm7593@naver.com>
2024-03-15 14:41:45 +0900

    컨트롤러 메인 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/MainPageController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/MainPageController.java
@@ -1,28 +1,16 @@
 package com.jarvis.BalanceGame.controller.user.page;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.jarvis.BalanceGame.model.dto.MemberDTO;
-import com.jarvis.BalanceGame.service.MemberService;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainPageController {
 
-	@Autowired MemberService memberService;
 	@GetMapping("/")
-	public @ResponseBody String mainPageController() {
+	public  String mainPageController() {
 	System.out.println("메인페이지 이동");
 	
-	MemberDTO dto = new MemberDTO();
-	dto.setSearchCondition("viewAll");
-	memberService.selectAll(dto);
 	
-		return "ok";
+		return "user/index";
 	}
 }
