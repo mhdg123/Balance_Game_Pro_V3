6dab74eaa93360a4af1a9bbed4bfaf00cd05ea05
조지훈 <whwlgns42@kakao.com>
2024-03-16 21:41:11 +0900

    sweetAlert 스크립트 추가

---
+++ BalanceGame/src/main/resources/static/common/js/alert.js
@@ -0,0 +1,21 @@
+// 경고 메세지 출력 함수
+	function showError(title, text) {
+	    Swal.fire({
+	        title: title,
+	        text: text,
+	        icon: "warning"
+	    });
+	}
+ 	
+	// 성공 메세지 출력 함수
+	function showSuccess(title, text) {
+	    Swal.fire({
+	        title: title,
+	        text: text,
+	        icon: "success"
+	    });
+	}
+	
+	function ttt() {
+		alert("fffff")
+	}
