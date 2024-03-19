b23b7940d27bee051152da2b21a9b86cf6a33b9e
조지훈 <whwlgns42@kakao.com>
2024-03-19 14:33:01 +0900

    카카오 로그인시 뷰페이지 분기 처리

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/action/LoginController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/action/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +12,7 @@ import com.jarvis.BalanceGame.model.dto.MemberDTO;
 import com.jarvis.BalanceGame.service.MemberService;
 
 import jakarta.servlet.http.HttpSession;
+import retrofit2.http.POST;
 
 @Controller
 @RequestMapping("/user")
@@ -48,4 +50,5 @@ public class LoginController {
 			return "alert";
 
 	}
+	
 }
--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/JoinPageController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/JoinPageController.java
@@ -1,8 +1,12 @@
 package com.jarvis.BalanceGame.controller.user.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jarvis.BalanceGame.model.dto.MemberDTO;
 
 @Controller
 @RequestMapping("/user")
@@ -13,4 +17,17 @@ public class JoinPageController {
       System.out.println("회원가입 페이지 이동");
       return "user/join";
    }
+	
+	
+	@PostMapping("/join/kakao")
+	public String kakaoJoin(MemberDTO mDTO, Model model) {
+		System.out.println(mDTO + "카카오로 처음 로그인 해서 회원가입 처리 ");
+		// 비회원일때
+		model.addAttribute("mDTO", mDTO);
+		model.addAttribute("status", "kakaoJoin");
+		return "user/join";
+		
+		// 데이터 insert는 회원가입페이지에서 처리할 예정
+		
+	}
 }
