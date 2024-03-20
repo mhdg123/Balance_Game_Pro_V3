7c8e977c21411b63380ce873fac78c156d2199cd
DESKTOP-HUJUVSS\Administrator <mhdg123@naver.com>
2024-03-20 09:21:48 +0900

    memberDTO 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/model/dto/MemberDTO.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/model/dto/MemberDTO.java
@@ -13,31 +13,20 @@ import lombok.ToString;
 
 @Data
 public class MemberDTO {
-	  @NotBlank
 	  private String loginId; // 로그인 ID pk
-	  @NotBlank
 	  private String memberPassword; // 비밀번호
-	  @NotBlank
 	  private String name; // 이름
-	  @NotBlank
 	  private String nickName;
-	  @NotNull
 	  private String cellPhone; // 전화번호
-	  @NotNull
 	  private String email; // 이메일
-	  @NotNull
 	  private String address; // 주소
-	  @NotNull
 	  private String gender; // 성별
 	  @DateTimeFormat(pattern="yyyy-MM-dd")
-	  @NotNull
 	  private String age; // 생년월일
 	  private int coin; // 포인트
 	  private int grade; // 등급
 	  private String advertisementStatus; // 광고 상태(제거인지 아닌지)
-	  @NotNull
 	  private String role; // 관리자 유무
-	  @NotNull
 	  private Date memberDate; // 유저 등록시간
 
 	  //----------------------------------------------
