d269dc44570430a15de918d3dbcf3a30def6ceb2
chanwoo1997 <hm7593@naver.com>
2024-03-20 10:29:20 +0900

    찜 비동기 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/WishAsync.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/WishAsync.java
@@ -1,47 +1,49 @@
-//package com.jarvis.BalanceGame.controller.user.async;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import com.jarvis.BalanceGame.model.dto.WishDTO;
-//import com.jarvis.BalanceGame.service.WishService;
-//
-//import jakarta.servlet.http.HttpSession;
-//
-//public class WishAsync {
-//
-//	@Autowired
-//	private WishService wishService;
-//	
-//	
-//	@RequestMapping("/wishAsync")
-//	public @ResponseBody String wishAsync(WishDTO wDTO, HttpSession session) {
-//		
-//		wDTO.setLoginId((String)session.getAttribute("loginId"));
-//		
-//		boolean flag=false; 
-//		String src="";
-//		if (wishService.selectOne(wDTO) == null) {
-//			flag = wishService.insert(wDTO);
-//			if (flag) {
-//				src="찜o.png";
-//			}
-//		}else {
-//			wDTO.setSearchCondition("qm찜삭제");
-//			flag= wishService.delete(wDTO);
-//			if (flag) {
-//				src="찜x.png";
-//			}
-//		}
-//
-//		if(!flag) {
-//			
-//			System.out.println("실패");
-//			return "실패";
-//		}
-//			System.out.println(src);
-//
-//		return src;
-//	}
-//}
+package com.jarvis.BalanceGame.controller.user.async;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jarvis.BalanceGame.model.dto.WishDTO;
+import com.jarvis.BalanceGame.service.WishService;
+
+import jakarta.servlet.http.HttpSession;
+@Controller
+@RequestMapping("/user")
+public class WishAsync {
+
+	@Autowired
+	private WishService wishService;
+	
+	
+	@PostMapping("/wishAsync")
+	public @ResponseBody String wishAsync(WishDTO wDTO, HttpSession session) {
+		
+		wDTO.setLoginId((String)session.getAttribute("loginId"));
+		
+		boolean flag=false; 
+		String src="";
+		if (wishService.selectOne(wDTO) == null) {
+			flag = wishService.insert(wDTO);
+			if (flag) {
+				src="wishOn";
+			}
+		}else {
+			flag= wishService.delete(wDTO);
+			if (flag) {
+				src="wishOff";
+			}
+		}
+
+		if(!flag) {
+			
+			System.out.println("실패");
+			return "실패";
+		}
+			System.out.println(src);
+
+		return src;
+	}
+}
