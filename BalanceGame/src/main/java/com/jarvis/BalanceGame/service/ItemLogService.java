7ce2bc81e9286f13a8159529e8bb702f4a26f525
전은주 <mhdg123@naver.com>
2024-03-20 17:28:47 +0900

    아이템로그 Service  완성

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/service/ItemLogService.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/service/ItemLogService.java
@@ -5,9 +5,6 @@ import java.util.List;
 import com.jarvis.BalanceGame.model.dto.ItemLogDTO;
 
 public interface ItemLogService {
-	public boolean insert();
-	public boolean update();
-	public boolean delete();
-	public List<ItemLogDTO> selectAll();
-	public ItemLogDTO selectOne();
+	public boolean insert(ItemLogDTO iDTO);
+	public List<ItemLogDTO> selectAll(ItemLogDTO iDTO);
 }
--- BalanceGame/src/main/java/com/jarvis/BalanceGame/service/ItemLogServiceImpl.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/service/ItemLogServiceImpl.java
@@ -1,8 +1,27 @@
 package com.jarvis.BalanceGame.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jarvis.BalanceGame.model.dao.ItemLogDAO;
+import com.jarvis.BalanceGame.model.dto.ItemLogDTO;
+
 @Service
-public class ItemLogServiceImpl {
+public class ItemLogServiceImpl implements ItemLogService{
+
+	@Autowired
+	private ItemLogDAO itemLogDAO;
+	
+	@Override
+	public boolean insert(ItemLogDTO iDTO) {
+		return itemLogDAO.insert(iDTO);
+	}
+
+	@Override
+	public List<ItemLogDTO> selectAll(ItemLogDTO iDTO) {
+		return itemLogDAO.selectAll(iDTO);
+	}
 
 }
