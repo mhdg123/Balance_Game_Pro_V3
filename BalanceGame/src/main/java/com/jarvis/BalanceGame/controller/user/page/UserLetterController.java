5693aef0158d311fe06c61b51182cdc72eb65315
chanwoo1997 <hm7593@naver.com>
2024-03-21 09:12:17 +0900

    건의사항 컨트롤러 Post 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/UserLetterController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/UserLetterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jarvis.BalanceGame.model.dto.LetterDTO;
@@ -18,7 +19,7 @@ public class UserLetterController {
 	@Autowired
 	private LetterService letterService;
 	
-	@GetMapping("/sendLetter")
+	@PostMapping("/sendLetter")
 	public String userSuggertionController(LetterDTO lDTO, Model model, HttpSession session) {
 		
 		String loginId = (String) session.getAttribute("loginId");
