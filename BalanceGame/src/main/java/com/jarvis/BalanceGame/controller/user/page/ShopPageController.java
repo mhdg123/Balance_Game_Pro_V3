d93b5a378435b3551562a0b1f854c3ad7c94a1b1
chanwoo1997 <hm7593@naver.com>
2024-03-21 13:49:46 +0900

    포인트상점 페이지 컨트롤러 수정( 로그인하고 페이지 이동 가능)

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/ShopPageController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/ShopPageController.java
@@ -19,35 +19,38 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/user")
 public class ShopPageController {
    
-//   @Autowired
-//   private ItemService itemService;
-//   
-//   @Autowired
-//   private MemberService memberService;
+   @Autowired
+   private ItemService itemService;
+   
+   @Autowired
+   private MemberService memberService;
+   
+  
 
-   @GetMapping("/shopPage")
+   @GetMapping("/shop")
    public String shopPageController(ItemDTO iDTO, MemberDTO mDTO, Model model, HttpSession session) {
       System.out.println("상점 페이지 이동");
       
-      // 페이지 이동시 selectAll
-      
-      // 아이템 PK
-      // 아이템 이름
-      // 아임템 가격
-      // 로그인 아이디
-      // 이미지 url
-      // 유저 가지고 있는 포인트
+//       페이지 이동시 selectAll
+//      
+//       아이템 PK
+//       아이템 이름
+//       아임템 가격
+//       로그인 아이디
+//       이미지 url
+//       유저 가지고 있는 포인트
 
       
-//      List<ItemDTO> data =  itemService.selectAll();
-//      mDTO.setSearchCondition(null);
+      List<ItemDTO> data =  itemService.selectAll(iDTO);
 //      MemberDTO loginData = memberService.selectOne(mDTO);
-//      model.addAttribute(data);
-//      model.addAttribute("loginId", loginData.getLoginId());
-//      model.addAttribute("point", loginData.getCoin());
+      model.addAttribute(data);
+      
+      String loginId = (String)session.getAttribute("loginId");
+      mDTO.setLoginId(loginId);
       
       
-//      
+      
+      
       return "user/shop";
    }
 }
