c1f964eab5fbda3510a8e07707ee12689be0ac7e
chanwoo1997 <hm7593@naver.com>
2024-03-22 11:18:11 +0900

    메일함 수정 컨트롤러

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/MailBoxAsync.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/MailBoxAsync.java
@@ -1,5 +1,7 @@
 package com.jarvis.BalanceGame.controller.user.async;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,4 +41,20 @@ public class MailBoxAsync {
 
 		return gson.toJson(letterService.selectOne(lDTO));
 	}
+	
+	@PostMapping("/mailCount")
+	public @ResponseBody String mailCount(LetterDTO lDTO, Model model, Gson gson, HttpSession session) {
+		String loginId = (String) session.getAttribute("loginId");
+	    lDTO.setLoginId(loginId);
+	
+	    letterService.update(lDTO);
+
+	    lDTO.setSearchCondition("unReadMessage");
+	    // 읽지 않은 편지 수를 조회하여 반환
+	    List<LetterDTO> unreadLetters = letterService.selectAll(lDTO);
+	    int unreadCount = unreadLetters.size();
+
+	    return Integer.toString(unreadCount);
+	}
+	
 }
