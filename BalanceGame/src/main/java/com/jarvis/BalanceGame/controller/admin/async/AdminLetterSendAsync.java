2a72c617850e3a4fb241443bc26b40eb60781460
chanwoo1997 <hm7593@naver.com>
2024-03-22 14:16:19 +0900

    관리자 건의사항 보내기 비동기 추가

---
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/async/AdminLetterSendAsync.java
@@ -0,0 +1,38 @@
+package com.jarvis.BalanceGame.controller.admin.async;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jarvis.BalanceGame.model.dto.LetterDTO;
+import com.jarvis.BalanceGame.service.LetterService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminLetterSendAsync {
+
+	@Autowired
+	private LetterService letterService;
+	
+	@GetMapping("/adminLetterSendAsync")
+	public @ResponseBody String adminLetterSendAsync(LetterDTO lDTO, HttpSession session) {
+		
+		String loginId = (String)session.getAttribute("loginId");
+		
+		lDTO.setLoginId(loginId);
+		
+		boolean flag = letterService.insert(lDTO);
+		
+		if(!flag) {
+			return "fail";
+		}
+		
+		return "success";
+	}
+	
+	
+}
