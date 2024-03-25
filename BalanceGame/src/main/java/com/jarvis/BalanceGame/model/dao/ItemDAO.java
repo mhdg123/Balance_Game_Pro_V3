ff48dfa462d55a500c56d71c33148dcd59fcd5fa
전은주 <mhdg123@naver.com>
2024-03-25 10:58:21 +0900

    아이템 타입 다시 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/model/dao/ItemDAO.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/model/dao/ItemDAO.java
@@ -17,7 +17,8 @@ public class ItemDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	private static final String SELECTALL = "SELECT ITEM_ID, ITEM_NAME, ITEM_PRICE, ITEM_IMAGE, ITEM_TYPE FROM ITEM WHERE ITEM_TYPE = ? ";
+	private static final String SELECTALL_ITEM_TYPE = "SELECT ITEM_ID, ITEM_NAME, ITEM_PRICE, ITEM_IMAGE FROM ITEM WHERE ITEM_TYPE = 'item' ";
+	private static final String SELECTALL_POINT_TYPE = "SELECT ITEM_ID, ITEM_NAME, ITEM_PRICE, ITEM_IMAGE FROM ITEM WHERE ITEM_TYPE = 'point' ";
 	private static final String SELECTONE = "SELECT ITEM_ID, ITEM_NAME, ITEM_PRICE, ITEM_IMAGE, ITEM_TYPE FROM ITEM WHERE ITEM_ID =?";
 	private static final String SELECTONE_NEXT_ID = "SELECT MAX(ITEM_ID)+1 AS NEXT_ITEM_ID FROM ITEM";
 	private static final String INSERT = "INSERT INTO ITEM (ITEM_NAME, ITEM_PRICE, ITEM_IMAGE, ITEM_TYPE) VALUES (?,?,?,?)";
@@ -26,8 +27,12 @@ public class ItemDAO {
 	
 	public List<ItemDTO> selectAll(ItemDTO iDTO){
 		List<ItemDTO> datas = null;
-		Object[] args = {iDTO.getItemType()};
-		datas = jdbcTemplate.query(SELECTALL, args, new ItemRowMapper());
+                        if(iDTO.getSearchCondition().equals("itemType")) {
+			datas = jdbcTemplate.query(SELECTALL_ITEM_TYPE, args, new ItemRowMapper());
+		}
+		else if((iDTO.getSearchCondition().equals("pointType")){
+			datas = jdbcTemplate.query(SELECTALL_POINT_TYPE, args, new ItemRowMapper());
+		}
 		return datas;
 	}
 	public ItemDTO selectOne(ItemDTO iDTO) {
@@ -80,7 +85,6 @@ class ItemRowMapper implements RowMapper<ItemDTO>{
 		datas.setItemName(rs.getString("ITEM_NAME"));
 		datas.setItemPrice(rs.getInt("ITEM_PRICE"));
 		datas.setItemImg(rs.getString("ITEM_IMAGE"));
-		datas.setItemType(rs.getString("ITEM_TYPE"));
 		return datas;
 	}
 }
