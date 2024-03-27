1079a147dc252b1ed5ac5bba38180da6380c1a7e
조지훈 <whwlgns42@kakao.com>
2024-03-27 11:24:02 +0900

    관리자 아이템 등록 파일 업로드 모듈화 적용

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/action/AdminItemCreateController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/action/AdminItemCreateController.java
@@ -1,7 +1,7 @@
 package com.jarvis.BalanceGame.controller.admin.action;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,56 +11,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.jarvis.BalanceGame.SavePictures;
 import com.jarvis.BalanceGame.model.dto.ItemDTO;
 import com.jarvis.BalanceGame.service.ItemService;
 
 import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/admin")
 @MultipartConfig
 public class AdminItemCreateController {
-	private static final String UPLOAD_DIR = "C:\\Balance_Game_Pro_V3\\BalanceGame\\src\\main\\resources\\static\\upload\\"; // 파일 업로드 절대경로
+	@Autowired
+	private SavePictures savePictures;
 
+	@Autowired
+	private ItemService itemService;
 
-    @Autowired
-    private ItemService itemService;
+	@PostMapping("/adminItemCreate")
+	public String adminItemCreateController(ItemDTO iDTO, Model model, @RequestParam("file") List<MultipartFile> files,
+			HttpServletRequest request) {
+		System.out.println("관리자 아이템 등록 데이터 : " + iDTO);
 
-    @PostMapping("/adminItemCreate")
-    public String adminItemCreateController(ItemDTO iDTO, Model model, @RequestParam("file") MultipartFile file) {
-        System.out.println("관리자 아이템 등록 데이터 : " + iDTO);
-        if (file.isEmpty()) {
-            return "파일이 비어 있습니다.";
-        }
-        try {
-            // 업로드 디렉토리가 없으면 생성
-            File uploadDir = new File(UPLOAD_DIR);
-            if (!uploadDir.exists()) {
-                uploadDir.mkdirs();
-            }
-            String fileName = file.getOriginalFilename();
-            // 업로드 파일 경로 설정
-            String filePath = UPLOAD_DIR + fileName;
-            File dest = new File(filePath);
-            iDTO.setItemImg(fileName);
-            boolean flag = itemService.insert(iDTO);
+		try {
+			List<String> fileNames = savePictures.storeImages(files, request.getServletContext().getRealPath("/"));
 
-            if (!flag) {
-                model.addAttribute("status", "fail");
-                model.addAttribute("msg", "아이템추가 실패했습니다");
-                model.addAttribute("redirect", "/admin/adminPage");
-                return "alert";
-            }
-            // 파일 저장
-            file.transferTo(dest);
-            model.addAttribute("status", "success");
-            model.addAttribute("msg", "아이템추가 성공했습니다");
-            model.addAttribute("redirect", "/admin/adminItemManagementPage");
-            return "alert";
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "파일 업로드 중 오류가 발생했습니다.";
-        }
+			// 이미지 파일명을 DTO에 설정
+			System.out.println(fileNames.get(0));
+			iDTO.setItemImg(fileNames.get(0));
+			boolean flag = itemService.insert(iDTO);
 
-    }
+			if (!flag) {
+				model.addAttribute("status", "fail");
+				model.addAttribute("msg", "아이템추가 실패했습니다");
+				model.addAttribute("redirect", "/admin/adminItemManagementPage");
+				return "alert";
+			}
+
+			model.addAttribute("status", "success");
+			model.addAttribute("msg", "성공");
+			model.addAttribute("redirect", "/admin/adminItemManagementPage");
+			return "alert";
+		} catch (IOException e) {
+			e.printStackTrace();
+			// 파일 업로드 실패 처리
+			model.addAttribute("status", "fail");
+			model.addAttribute("msg", "파일 업로드 실패");
+			model.addAttribute("redirect", "/admin/adminItemManagementPage");
+			return "alert";
+		}
+
+	}
 }
