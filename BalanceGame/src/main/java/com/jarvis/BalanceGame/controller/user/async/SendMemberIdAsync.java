33451af955b8cd1d93bc521109a4102adefa7fdf
전은주 <mhdg123@naver.com>
2024-03-27 17:37:41 +0900

    아이디 찾기 구현중

---
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/SendMemberIdAsync.java
@@ -0,0 +1,10 @@
+package com.jarvis.BalanceGame.controller.user.async;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class SendMemberIdAsync {
+	
+}
