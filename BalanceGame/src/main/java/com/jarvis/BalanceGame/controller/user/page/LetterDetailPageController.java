329fa9e023e17461980468282afa1f19abf0bc30
chanwoo1997 <hm7593@naver.com>
2024-03-29 16:09:16 +0900

    메세지 상세페이지 컨트롤러 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/LetterDetailPageController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/page/LetterDetailPageController.java
@@ -21,6 +21,8 @@ public class LetterDetailPageController {
 		
 		
 		lDTO = letterService.selectOne(lDTO);
+		lDTO.setSearchCondition("updateReadStatus");
+		letterService.update(lDTO);
 		
 		model.addAttribute("letterData",lDTO);
 		
