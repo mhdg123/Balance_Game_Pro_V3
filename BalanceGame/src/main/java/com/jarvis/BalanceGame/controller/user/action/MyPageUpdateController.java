84b0dce3e5d3a9680b9b25748fdbe2f5dd2219d6
chanwoo1997 <hm7593@naver.com>
2024-04-01 15:41:00 +0900

    닉네임 변경권 업데이트 컨트롤러 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/action/MyPageUpdateController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/action/MyPageUpdateController.java
@@ -16,46 +16,45 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/user")
 public class MyPageUpdateController {
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@Autowired
 	private MemberItemService memberItemService;
-	
+
 	@GetMapping("/myInfoChangePage")
-	public String MyPageUpdateController(MemberDTO mDTO, MemberItemDTO miDTO, Model model,HttpSession session) {
-		model.addAttribute("UTF-8");
-		mDTO.setLoginId((String)session.getAttribute("loginId"));
+	public String MyPageUpdateController(MemberDTO mDTO, MemberItemDTO miDTO, Model model, HttpSession session) {
+		String loginId = (String)session.getAttribute("loginId");
+		mDTO.setLoginId(loginId);
 		mDTO.setSearchCondition("viewOne");
 		memberService.selectOne(mDTO);
-		
+
 		String oldName = mDTO.getNickName();
-		
-		
+
 		mDTO.setSearchCondition("midifyMyInfo");
-		if(!memberService.update(mDTO)) {
+		if (!memberService.update(mDTO)) {
 			System.out.println("내정보 변경 실패");
-			model.addAttribute("status", "fail");			
+			model.addAttribute("status", "fail");
 			model.addAttribute("msg", "정보 변경에 실패하였습니다.");
 			model.addAttribute("redirect", "myInfo");
 			return "alert";
-			
+
 		}
-			System.out.println("내정보 변경 성공");
-			model.addAttribute("status", "success");			
-			model.addAttribute("msg","정보변경 성공");
-			model.addAttribute("redirect", "");
-			
-			if(oldName != mDTO.getNickName()) {
-				
-				miDTO.setSearchCondition("useItem");
-				memberItemService.update(miDTO);
-				
-			}
-			
+
+		System.out.println("내정보 변경 성공");
+		model.addAttribute("status", "success");
+		model.addAttribute("msg", "정보변경 성공");
+		model.addAttribute("redirect", "");
+
+		if (oldName != mDTO.getNickName()) {
+
+			miDTO.setSearchCondition("useItem");
+			memberItemService.update(miDTO);
 			
 		return "alert";
+		}
+		return "alert";
 	}
-	
+
 }
