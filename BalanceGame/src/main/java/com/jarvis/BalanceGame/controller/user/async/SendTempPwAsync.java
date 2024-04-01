a5ce41c26fb937650248f770d6871d185308688b
chanwoo1997 <hm7593@naver.com>
2024-04-01 16:04:17 +0900

    비번찾기 패스워드 컨트롤러 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/SendTempPwAsync.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/user/async/SendTempPwAsync.java
@@ -12,6 +12,11 @@ import com.jarvis.BalanceGame.model.dto.MemberDTO;
 import com.jarvis.BalanceGame.service.MemberService;
 import com.jarvis.BalanceGame.service.SendTempPwService;
 
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+
 @Controller
 @RequestMapping("/user")
 public class SendTempPwAsync {
@@ -39,5 +44,42 @@ public class SendTempPwAsync {
 			}
 		}
 		return false;
+	}
+	
+	@PostMapping("sendMemberPwNumberAsync")
+	public @ResponseBody String sendMemberPwNumberAsync(@RequestBody MemberDTO mDTO) {
+		System.out.println("전화번호로 비번 찾기 이름 데이터 : " + mDTO.getLoginId());
+		System.out.println("전화번호로 아이디 찾기 전화번호 데이터 : " + mDTO.getCellPhone());
+		System.out.println("문자 인증 기능 실행");
+
+		mDTO.setSearchCondition("isTempPwInfoCorrectCellPhone");
+		mDTO = memberService.selectOne(mDTO);
+		if(mDTO.getLoginType().equals("SOCIAL")) {
+			return "social";
+		}
+
+		String phone = mDTO.getCellPhone();
+		String userPassword = mDTO.getMemberPassword();
+		if(mDTO != null) {
+		DefaultMessageService messageService = NurigoApp.INSTANCE.initialize("NCS6ZHPOOM5UZPWE",
+				"UG5ONIDCC9V05JCNXNXFSMYYZOMHCYOH", "https://api.coolsms.co.kr"); // Message
+		Message message = new Message();
+		message.setFrom("01087937953");
+		message.setTo(phone);
+		message.setText(userPassword);
+
+		try { // send 메소드로 ArrayList<Message> 객체를 넣어도 동작합니다!
+			messageService.send(message);
+		} catch (NurigoMessageNotReceivedException exception) { // 발송에 실패한 메시지 목록을 확인할 수 있습니다!
+			System.out.println(exception.getFailedMessageList());
+			System.out.println(exception.getMessage());
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
+
+		System.out.println("문자 아이디: " + userPassword);
+		return "success";
+		}
+		return "fail";
 	}
 }
