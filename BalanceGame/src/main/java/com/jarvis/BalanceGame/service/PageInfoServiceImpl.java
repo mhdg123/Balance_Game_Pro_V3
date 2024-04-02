d6f98a9569ff799bf3ee1bcd14a1d9fa21bd76aa
전은주 <mhdg123@naver.com>
2024-04-02 11:44:07 +0900

    @서비스 추가

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/service/PageInfoServiceImpl.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/service/PageInfoServiceImpl.java
@@ -3,11 +3,13 @@ package com.jarvis.BalanceGame.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.jarvis.BalanceGame.model.dao.PageInfoDAO;
 import com.jarvis.BalanceGame.model.dto.PageInfoDTO;
 import com.jarvis.BalanceGame.model.dto.QuestionDTO;
 
+@Service
 public class PageInfoServiceImpl implements PageInfoService{
 
 	@Autowired
