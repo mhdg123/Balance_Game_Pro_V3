1ce59fa37131a9a9a6a52e12254b04070c0ba0d5
전은주 <mhdg123@naver.com>
2024-04-03 13:37:57 +0900

    토탈 서비스 구현

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/service/TotalService.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/service/TotalService.java
@@ -5,9 +5,5 @@ import java.util.List;
 import com.jarvis.BalanceGame.model.dto.TotalDTO;
 
 public interface TotalService {
-	public boolean insert();
-	public boolean update();
-	public boolean delete();
-	public List<TotalDTO> selectAll();
-	public TotalDTO selectOne();
+	public List<TotalDTO> selectAll(TotalDTO tDTO);
 }
--- BalanceGame/src/main/java/com/jarvis/BalanceGame/service/TotalServiceImpl.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/service/TotalServiceImpl.java
@@ -1,8 +1,20 @@
 package com.jarvis.BalanceGame.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.jarvis.BalanceGame.model.dao.TotalDAO;
+import com.jarvis.BalanceGame.model.dto.TotalDTO;
+
 @Service
-public class TotalServiceImpl {
+public class TotalServiceImpl implements TotalService{
+
+	private TotalDAO totalDAO;
+	
+	@Override
+	public List<TotalDTO> selectAll(TotalDTO tDTO) {
+		return totalDAO.selectAll(tDTO);
+	}
 
 }
