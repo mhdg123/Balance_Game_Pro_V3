37e8edf68b5180d3403c38a86bce30e2958b194e
chanwoo1997 <hm7593@naver.com>
2024-04-03 14:51:09 +0900

    페이징 컨트롤러 수정

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/page/AdminQuestionAccessPageController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/page/AdminQuestionAccessPageController.java
@@ -8,8 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jarvis.BalanceGame.model.dto.PageInfoDTO;
 import com.jarvis.BalanceGame.model.dto.QuestionDTO;
+import com.jarvis.BalanceGame.service.PageInfoService;
 import com.jarvis.BalanceGame.service.QuestionService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,26 +22,48 @@ public class AdminQuestionAccessPageController {
 	@Autowired
 	private QuestionService questionService;
 	
+	@Autowired
+	private PageInfoService pageInfoService;
 	
 	@GetMapping("/questionAccessPage")
-	public String adminTitleAccessPageController(QuestionDTO qDTO, Model model) {
+	public String adminTitleAccessPageController(QuestionDTO qDTO, PageInfoDTO pDTO, Model model, HttpSession session) {
 		
-		qDTO.setSearchCondition("adminViewAllOfUnapprovedQuestions");
-		
-		qDTO.setQuestionAccess("F");
-		
-		List<QuestionDTO> qdatas_f = questionService.selectAll(qDTO);
-		
-		System.out.println("questionDatas_f"+qdatas_f);
-		
-		if(qdatas_f == null) {	
-			model.addAttribute("status", "fail");
-			model.addAttribute("msg", "해당 데이터가 없습니다");
-			model.addAttribute("redirect", "adminQuestionAccess");
-			return "alert";
+		String loginId = (String)session.getAttribute("loginId");
+		if(pDTO.getCurrentPage() == 0) {
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<");
+			pDTO.setCurrentPage(1);
 		}
 		
-		model.addAttribute("questionDatas_f", qdatas_f);
+		pDTO.setLoginId(loginId);
+		pDTO.setPasingnationSize(10);
+		
+		pDTO.setOffset(pageInfoService.calculateOffset(pDTO));
+		System.out.println(pDTO.getOffset());
+		pDTO.setSearchCondition("adminViewAllOfUnapprovedQuestions");
+		System.out.println("pDTO" + pDTO);
+		List<PageInfoDTO> datas = pageInfoService.selectAll(pDTO);
+		
+		qDTO.setQuestionAccess("F");
+		qDTO.setSearchCondition("questionCount");
+		qDTO = questionService.selectOne(qDTO);
+		System.out.println(qDTO);
+		pDTO.setTotalRows(qDTO.getQuestionCount());
+		int totalPage = pageInfoService.calcTotalPages(pDTO);	// 총페이지 수
+		
+
+		System.out.println(totalPage);
+		
+		if(datas != null) {
+			model.addAttribute("questionDatas_f", datas);
+			model.addAttribute("totalPage", totalPage);
+			model.addAttribute("page", pDTO.getCurrentPage());
+		}else {
+			model.addAttribute("status", "fail");
+			model.addAttribute("msg", "등록된 문제가 없습니다.");
+			model.addAttribute("redirect", "");
+			return "alert";
+		}
+
 
 		return "admin/adminQuestionAccess";
 	}
--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/page/AdminQuestionManagementPageController.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/page/AdminQuestionManagementPageController.java
@@ -8,8 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jarvis.BalanceGame.model.dto.PageInfoDTO;
 import com.jarvis.BalanceGame.model.dto.QuestionDTO;
+import com.jarvis.BalanceGame.service.PageInfoService;
 import com.jarvis.BalanceGame.service.QuestionService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,24 +22,49 @@ public class AdminQuestionManagementPageController {
 	@Autowired
 	private QuestionService questionService;
 	
+	@Autowired
+	private PageInfoService pageInfoService;
+	
 	@GetMapping("/questionManagementPage")
-	public String adminTitleManagementPageController(QuestionDTO qDTO, Model model) {
+	public String adminTitleManagementPageController(QuestionDTO qDTO, PageInfoDTO pDTO, Model model,HttpSession session) {
 		
-		qDTO.setSearchCondition("adminViewAllOfApprovedQuestions");
+		String loginId = (String)session.getAttribute("loginId");
+		if(pDTO.getCurrentPage() == 0) {
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<");
+			pDTO.setCurrentPage(1);
+		}
+		
+		pDTO.setLoginId(loginId);
+		pDTO.setPasingnationSize(10);
+		
+		pDTO.setOffset(pageInfoService.calculateOffset(pDTO));
+		System.out.println(pDTO.getOffset());
+		pDTO.setSearchCondition("adminViewAllOfApprovedQuestions");
+		System.out.println("pDTO" + pDTO);
+		List<PageInfoDTO> datas = pageInfoService.selectAll(pDTO);
+		
+		
 		qDTO.setQuestionAccess("T");
+		qDTO.setSearchCondition("questionCount");
+		qDTO = questionService.selectOne(qDTO);
+		System.out.println(qDTO);
+		pDTO.setTotalRows(qDTO.getQuestionCount());
+		int totalPage = pageInfoService.calcTotalPages(pDTO);	// 총페이지 수
 		
+
+		System.out.println(totalPage);
 		
-		List<QuestionDTO> qdatas_t = questionService.selectAll(qDTO);
-		
-		System.out.println("questionDatas" + qdatas_t);
-		if(qdatas_t == null) {
+		if(datas != null) {
+			model.addAttribute("questionDatas", datas);
+			model.addAttribute("totalPage", totalPage);
+			model.addAttribute("page", pDTO.getCurrentPage());
+		}else {
 			model.addAttribute("status", "fail");
-			model.addAttribute("msg", "해당 데이터가 없습니다");
-			model.addAttribute("redirect", "adminPage");
+			model.addAttribute("msg", "등록된 문제가 없습니다.");
+			model.addAttribute("redirect", "");
 			return "alert";
 		}
-		model.addAttribute("questionDatas", qdatas_t);
-		System.out.println("questionDatas" + qdatas_t);
+
 		return "admin/adminQuestionManagement";
 	}
 }
