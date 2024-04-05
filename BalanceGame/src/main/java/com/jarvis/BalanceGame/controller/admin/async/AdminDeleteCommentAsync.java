8fd751fa9553bdc5f1d99d78ba12ff643f0c5108
chanwoo1997 <hm7593@naver.com>
2024-04-05 10:12:07 +0900

    신고 상세 페이지 댓글 삭제 컨트롤러 추가

--- BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/async/AdminDeleteCommentAsync.java
+++ BalanceGame/src/main/java/com/jarvis/BalanceGame/controller/admin/async/AdminDeleteCommentAsync.java
@@ -2,15 +2,14 @@ package com.jarvis.BalanceGame.controller.admin.async;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jarvis.BalanceGame.model.dto.CommentDTO;
+import com.jarvis.BalanceGame.model.dto.WarningDTO;
 import com.jarvis.BalanceGame.service.CommentService;
-
-import jakarta.servlet.http.HttpSession;
+import com.jarvis.BalanceGame.service.WarningService;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,12 +17,31 @@ public class AdminDeleteCommentAsync {
 
 	@Autowired
 	private CommentService commentService;
-
+	
+	@Autowired
+	private WarningService warningService;
+	
 	@PostMapping("/deleteAdminCommentAsync")
 	public @ResponseBody String deleteAdminCommentAsync(CommentDTO cDTO) {
 		System.out.println("관리자 댓글 삭제 파라미터 데이터 : " + cDTO);
 		cDTO.setSearchCondition("adminCommentDelete");
 		boolean flag = commentService.delete(cDTO);
+		if (!flag) {
+			return "fail";
+		}
+		return "success";
+
+	}
+	
+	@PostMapping("/warningDeleteCommentAsync")
+	public @ResponseBody String warningDeleteCommentAsync(CommentDTO cDTO, WarningDTO wDTO) {
+		System.out.println("관리자 댓글 삭제 파라미터 데이터 : " + cDTO);
+		cDTO.setSearchCondition("adminCommentDelete");
+		commentService.delete(cDTO);
+		int cId = cDTO.getCommentId();
+		wDTO.setCommentId(cId);
+		boolean flag = warningService.delete(wDTO);
+		
 		if (!flag) {
 			return "fail";
 		}
